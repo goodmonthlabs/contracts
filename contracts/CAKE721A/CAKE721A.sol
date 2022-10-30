63b9d2622c93080dc0c6fbce6343acd52dc6f624
0xfcks <david@goodmonth.io>
2022-10-30 08:58:54 -0700

    Added setters to ERC721A

--- contracts/CAKE721A/CAKE721A.sol
+++ contracts/CAKE721A/CAKE721A.sol
@@ -4,27 +4,33 @@ pragma solidity ^0.8.4;
 
 import "erc721a/contracts/ERC721A.sol";
 import "erc721a/contracts/extensions/ERC721AQueryable.sol";
+import "erc721a/contracts/extensions/ERC721ABurnable.sol";
 import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
 
-contract CAKE721A is ERC721A, ERC721AQueryable {
+contract CAKE721A is ERC721A, ERC721AQueryable, ERC721ABurnable {
 
   uint256 public MAX_TOKEN_SUPPLY;
   uint256 public MAX_TOTAL_MINTS_BY_ADDRESS;
   uint256 public MAX_TXN_MINT_LIMIT;
   uint256 public PRIVATE_SALE_TIMESTAMP;
   uint256 public PUBLIC_SALE_TIMESTAMP;  
-  bytes32 public MERKLEROOT;
+  bytes32 public MERKLEROOT;  
+  uint256 public PRICE;
+  string public PROVENANCE_HASH;
+  string public BASE_URI = '';
 
   constructor(
     string memory name, 
     string memory symbol, 
     uint256 supply,
+    uint256 price,
     uint256 maxTotalMints,
     uint256 maxTxnMints,
     uint256 privateSaleTimestamp,
     uint256 publicSaleTimestamp   
     ) ERC721A(name, symbol) {
       MAX_TOKEN_SUPPLY = supply;
+      PRICE = price;
       MAX_TOTAL_MINTS_BY_ADDRESS = maxTotalMints;
       MAX_TXN_MINT_LIMIT = maxTxnMints;
       PRIVATE_SALE_TIMESTAMP = privateSaleTimestamp;
@@ -32,14 +38,20 @@ contract CAKE721A is ERC721A, ERC721AQueryable {
   }
 
   function mint(address to, uint256 quantity, bytes32[] calldata proof) external payable {
-    string memory eligibilityCheck = checkMintEligibilityMethod(to, quantity, proof); 
+    string memory eligibilityCheck = checkMintEligibilityMethod(to, quantity, proof, msg.value); 
     require(bytes(eligibilityCheck).length==0, eligibilityCheck);
     _mint(to, quantity);
   }
 
-  function checkMintEligibilityMethod(address to, uint256 quantity, bytes32[] calldata proof) public view returns(string memory) {
+  function reserveTokens(address to, uint256 quantity) external {  
+    require(totalSupply() + quantity <= MAX_TOKEN_SUPPLY, 'Purchase exceeds max token supply.');    
+    _mint(to, quantity);
+  }
+  
+  function checkMintEligibilityMethod(address to, uint256 quantity, bytes32[] calldata proof, uint256 value) public view returns(string memory) {
     
     require(block.timestamp >= PRIVATE_SALE_TIMESTAMP || block.timestamp >= PUBLIC_SALE_TIMESTAMP, 'Sale is not active.');
+    
     if(block.timestamp < PUBLIC_SALE_TIMESTAMP){
       require(verifyWhitelistMembership(proof, to), "Wallet is not on the whitelist.");
     }
@@ -52,17 +64,63 @@ contract CAKE721A is ERC721A, ERC721AQueryable {
 
     if(MAX_TOTAL_MINTS_BY_ADDRESS > 0){
      require(balanceOf(to) + quantity <= MAX_TOTAL_MINTS_BY_ADDRESS, 'Purchase exceeds max total mints by address.'); 
-    }    
+    }
+
+    if(PRICE > 0){
+      require(value >= PRICE * quantity , 'Payment is below mint price.');
+    }
           
     return '';
   }
 
   function verifyWhitelistMembership(bytes32[] calldata proof, address _address) internal view returns (bool){        
-      bytes32 leaf = keccak256(abi.encodePacked(_address));        
-      return MerkleProof.verify(proof, MERKLEROOT, leaf);
+    bytes32 leaf = keccak256(abi.encodePacked(_address));        
+    return MerkleProof.verify(proof, MERKLEROOT, leaf);
   }
 
   function setMerkleroot(bytes32 merkleroot) external {
-      MERKLEROOT = merkleroot;
+    MERKLEROOT = merkleroot;
   }
+
+  function setTotalSupply(uint256 supply) external {
+    require(supply >= totalSupply(), 'Cannot set supply to be lower than existing supply.');
+    MAX_TOKEN_SUPPLY = supply;
+  }
+
+  function setMaxTotalMintsByAddrss(uint256 max) external {
+    MAX_TOTAL_MINTS_BY_ADDRESS = max;
+  }
+
+  function setMaxMintsPerTxn(uint256 max) external {
+    MAX_TXN_MINT_LIMIT = max;
+  }
+
+  function setPrice(uint256 price) external {
+    require(price >= 0, 'Invalid price.');
+    PRICE = price;
+  }
+
+  function setPrivateSaleTimestamp(uint256 timestamp) external {
+    require(timestamp >= 0, 'Invalid timestamp.');
+    PRIVATE_SALE_TIMESTAMP = timestamp;
+  }
+
+  function setPublicSaleTimestamp(uint256 timestamp) external {
+    require(timestamp >= 0, 'Invalid timestamp.');
+    PUBLIC_SALE_TIMESTAMP = timestamp;
+  }
+
+  function setProvenanceHash(string calldata provenanceHash) external {
+    require(bytes(PROVENANCE_HASH).length==0, 'Provenance hash has already been set.');
+    PROVENANCE_HASH = provenanceHash;
+  }
+
+  function _baseURI() internal view override returns (string memory) {
+    return BASE_URI;
+  }
+
+  function setBaseURI(string calldata baseURI) external {
+    BASE_URI = baseURI;
+  }
+
 }
--- test/CAKE721A.js
+++ test/CAKE721A.js
@@ -19,7 +19,7 @@ describe("CAKE721A", function () {
     const [owner, otherAccount] = await ethers.getSigners();
 
     const Contract = await ethers.getContractFactory("CAKE721A");
-    const contract = await Contract.deploy("CAKE", "CAKE", 10, 0, 0, 1667071242, 1667071242);
+    const contract = await Contract.deploy("CAKE", "CAKE", 10, 5000000000000000, 0, 0, 1667071242, 1667171242);
 
     return { contract, owner, otherAccount };
   }
@@ -39,7 +39,7 @@ describe("CAKE721A", function () {
       expect(merkleroot).to.equal("0xa645c2ad6d07684f6b06a4c1cb6cd9e70bcce6fe256ce6bd997150af0d73c9fa")
     })
 
-    it("Mint", async () => {
+    it.skip("Mint", async () => {
       const { contract } = await loadFixture(deployOneYearLockFixture);
       await contract.setMerkleroot("0xa645c2ad6d07684f6b06a4c1cb6cd9e70bcce6fe256ce6bd997150af0d73c9fa")     
       
@@ -54,12 +54,45 @@ describe("CAKE721A", function () {
       "0x8d3364a8ba45c8d262d19fdc488c56f4037ad81b32debd6d0de38280da145efc",
       "0x972ab3bd87060b3f56119282d88947306d75bb4688bc3e9401e87fa40b698e2a"]
 
-      await contract.mint("0x29c6a598a3447F69ff52b9b96dadf630750886FD", 10, [])
+      const options = {value: ethers.utils.parseEther("1")}
+      await contract.mint("0x29c6a598a3447F69ff52b9b96dadf630750886FD", 1, proof, options)
       balanceOf = await contract.balanceOf("0x29c6a598a3447F69ff52b9b96dadf630750886FD")
-      expect(balanceOf).to.equal(10)
+      expect(balanceOf).to.equal(1)
 
       tokens = await contract.tokensOfOwner("0x29c6a598a3447F69ff52b9b96dadf630750886FD")
       console.log(tokens)
+    })
+
+    it("Set new baseURI and call it with tokenURI", async () => {
+      const { contract } = await loadFixture(deployOneYearLockFixture);
+      await contract.setBaseURI('cake.xyz/tokens/')
+      await contract.setMerkleroot("0xa645c2ad6d07684f6b06a4c1cb6cd9e70bcce6fe256ce6bd997150af0d73c9fa")     
+      
+      // compute proof offline
+      proof = ["0x0334329dc89b931f74e4f1cc74a37a9f24e6bcf4637bfa519bdf52dc4a4ffc81",
+      "0xa57d2faee7e110421a56977b75a6b670be5d080916decca93f2b0d6c2037faf2",
+      "0x833a5e64df2a9f59f1ddb8f07dece5df2cd2ca75190f470e0992f1863ed74c2d",
+      "0x10f8d80c548d3fb632124cd02f050757d4e7c8a7e3f56d54ec581851ce4364e0",
+      "0xd7d612fafeb49945e3ea421720a5157616dce38e53dd0873288221d4d97de692",
+      "0x0e31dfbbce44687da63825de6b4f7f6f78619d35b01fc1280ca914ca09e057b0",
+      "0x1e4cb984f7ea4be53b1f2dbc8e29319acb2d7bd2b98c8da0f0fea34cb686a401",
+      "0x8d3364a8ba45c8d262d19fdc488c56f4037ad81b32debd6d0de38280da145efc",
+      "0x972ab3bd87060b3f56119282d88947306d75bb4688bc3e9401e87fa40b698e2a"]
+
+      const options = {value: ethers.utils.parseEther("1")}
+      await contract.mint("0x29c6a598a3447F69ff52b9b96dadf630750886FD", 1, proof, options)
+      
+      const tokenId = 0
+      tokenURI = await contract.tokenURI(tokenId)
+      expect(tokenURI).to.equal(`cake.xyz/tokens/${tokenId}`)      
+
+    })
+    
+    it('Reserve tokens', async () => {
+      const { contract } = await loadFixture(deployOneYearLockFixture);
+      await contract.reserveTokens("0x29c6a598a3447F69ff52b9b96dadf630750886FD", 10)
+      const balance = await contract.balanceOf("0x29c6a598a3447F69ff52b9b96dadf630750886FD")
+      expect(balance).to.equal(10)
     })
   });
 });
