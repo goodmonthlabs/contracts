65bab7f9f36256729dfd6cc0a37384937ca6593f
0xfcks <david@goodmonth.io>
2022-11-01 16:00:59 -0700

    Added deploy arguments file

---
+++ scripts/arguments.js
@@ -0,0 +1,12 @@
+const client = "0x29c6a598a3447F69ff52b9b96dadf630750886FD" 
+
+module.exports = [
+  ["CAKE","CAKE"],
+  [10,5000000000000000,0,0],
+  [1667071242, 1667171242],
+  client,
+  [client],
+  [1],
+  client,
+  50
+];
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -8,7 +8,7 @@ async function main() {
 
   const Token = await ethers.getContractFactory("CAKE721A");   
   const client = "0x29c6a598a3447F69ff52b9b96dadf630750886FD" 
-  const token = await Token.deploy(["CAKE","CAKE"], [10,5000000000000000,0,0], [1667071242, 1667171242], client, [], [], client, 50);
+  const token = await Token.deploy(["CAKE","CAKE"], [10,5000000000000000,0,0], [1667071242, 1667171242], client, [client], [1], client, 50);
 
   console.log("Token address:", token.address);
 }
